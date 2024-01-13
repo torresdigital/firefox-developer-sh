4c282f27e4c84f4a95ed4038037ae52c36b27aac
Anderson Torres <atorresnh@gmail.com>
2024-01-13 11:25:17 -0300

    Update firefox-developer.sh

--- firefox-developer.sh
+++ firefox-developer.sh
@@ -264,7 +264,7 @@ sleep 2
  ✅ Exec=/opt/firefox-developer/firefox\n 
  ✅ Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n 
  ✅ ype=Application\n 
- ✅ Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop && \
+ ✅ Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop && \ ${nc}
 
 echo
 echo -e " Êxito !"
