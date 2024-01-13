2ed1d16141d7124989d752bcaed11285bc11ca52
Anderson Torres <atorresnh@gmail.com>
2024-01-13 02:28:25 -0300

    Update firefox-developer.sh

--- firefox-developer.sh
+++ firefox-developer.sh
@@ -255,7 +255,8 @@ echo " 🐧 Criando novo icone de atalho do firefox-dev na seção de Snaps"
 echo
 
 sleep 2
- echo -ne ' ${b_green}
+ echo -ne  ${b_green}'
+ 
  ✅ [Desktop Entry]\n 
  ✅ Version=yy.y.y\n 
  ✅ Encoding=UTF-8\n 
@@ -264,7 +265,9 @@ sleep 2
  ✅ Exec=/opt/firefox-developer/firefox\n 
  ✅ Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n 
  ✅ ype=Application\n 
- ✅ Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop && \
+ ✅ Categories=Network'
+ 
+  | sudo tee /usr/share/applications/firefox-developer.desktop && \
 
 echo
 echo -e " Êxito !"
