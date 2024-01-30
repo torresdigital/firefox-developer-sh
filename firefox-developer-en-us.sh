a0051a9c93ccf6bd6d3417d6f67732278f307fba
Anderson Torres <atorresnh@gmail.com>
2024-01-30 16:56:37 -0300

    Update firefox-developer-en-us.sh

--- firefox-developer-en-us.sh
+++ firefox-developer-en-us.sh
@@ -282,6 +282,8 @@ echo
 echo " 🐧 Creating the shortcut icon to run Firefox Dev on Snaps (( snApps )) user section"
 echo
 
+echo -ne ${b_green}
+
 sleep 2
 echo -e '[Desktop Entry]\n Version=yy.y.y\n Encoding=UTF-8\n Name=Mozilla Firefox\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop && \
 
