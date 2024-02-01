320eee5bfa138fd421fc47b20769f4ba9b883635
Anderson Torres <atorresnh@gmail.com>
2024-02-01 13:57:04 -0300

    Update firefox-developer-pt-br.sh

--- firefox-developer-pt-br.sh
+++ firefox-developer-pt-br.sh
@@ -281,7 +281,6 @@ echo
 
 echo -ne ${b_green}
 
-
 sleep 2
  echo -e ${b_green} '[Desktop Entry]\n Version=yy.y.y\n Encoding=UTF-8\n Name=Mozilla Firefox\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop && \
 
